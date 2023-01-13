a7380a8796666a29c426632bcdb160d12313b3ff
Jeongmo <wjdah1402@gmail.com>
2023-01-13 17:15:36 +0900

    Practice input and ouput number

---
+++ Chapter_1/Test-cpp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout << "plz type any number that you want to output";
+    cin >> number; // Input number from keyboard
+    cout << "Number is " << number << "\n";
+}
