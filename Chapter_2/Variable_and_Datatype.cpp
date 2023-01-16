427e6954232f61514c1930b3dc897992f34f0109
Jeongmo <wjdah1402@gmail.com>
2023-01-16 16:25:53 +0900

    Output number with behind decimal point with <iomanip>

--- Chapter_2/Variable_and_Datatype.cpp
+++ Chapter_2/Variable_and_Datatype.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip> // For show 6 number behind decimal point
 #include <bitset> // For output binary form
 
 using namespace std;
@@ -8,6 +9,15 @@ int main()
     const double j = 00000011; // not Binary, This is Octal ex) 00000011 = 9
     cout << "number is " << j << endl;
     cout << "number is " << bitset<4>(j) << endl; // output binary form with 4 size
+
+    int vvi;
+    float vvf;
+    double vvd;
+    vvi = vvf = vvd = 12.3456789;
+    cout << setiosflags(ios::fixed); // if you don't write this, output 6 numbers that combine exponent and behind the decimal point
+    cout << "vvi = " << vvi << endl;
+    cout << "vvf = " << vvf << endl;
+    cout << "vvd = " << setprecision(10) << vvd << endl; // setprecision(10) is show 10 number behind decimal point in double type
 
     bool a; // bool is true or fulse 
     a = 100;
