907e894e96981819aac1b307a796ed812e6192c9
Jeongmo <wjdah1402@gmail.com>
2023-01-16 15:06:06 +0900

    Output size of each data type

---
+++ Chapter_2/Variable_and_Datatype.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "char : " << sizeof(char) << "byte.\n";
+    cout << "short : " << sizeof(short) << "byte.\n";
+    cout << "int : " << sizeof(int) << "byte.\n";
+    cout << "long : " << sizeof(long) << "byte.\n";
+    cout << "unsigned : " << sizeof(unsigned) << "byte.\n";
+    cout << "float : " << sizeof(float) << "byte.\n";
+    cout << "double : " << sizeof(double) << "byte.\n";
+    cout << "long double : " << sizeof(long double) << "byte.\n";
+}
