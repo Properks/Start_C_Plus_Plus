629b6d6e4bb8169b1941ebbf29f940f40c6ce5da
Jeongmo <wjdah1402@gmail.com>
2023-01-16 16:24:50 +0900

    Output character with calculate

--- Chapter_2/Variable_and_Datatype.cpp
+++ Chapter_2/Variable_and_Datatype.cpp
@@ -8,4 +8,13 @@ int main()
     const double j = 00000011; // not Binary, This is Octal ex) 00000011 = 9
     cout << "number is " << j << endl;
     cout << "number is " << bitset<4>(j) << endl; // output binary form with 4 size
+
+    bool a; // bool is true or fulse 
+    a = 100;
+    cout << "That is " << a << endl; // Result is 1 not true
+    
+    char Ch1 = 'A', Ch2 = 'a';
+    cout << oct << Ch1 + 1 << endl;
+    cout << oct << Ch2 + 1 << endl;
+    cout << "A - a -> " << Ch2 - Ch1 << endl;
 }
