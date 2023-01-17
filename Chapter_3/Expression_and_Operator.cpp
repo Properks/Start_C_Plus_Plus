15edac863ecc0c47a6e04d0003c708938cdc1b38
Jeongmo <wjdah1402@gmail.com>
2023-01-17 15:09:01 +0900

    Output each digit number

--- Chapter_3/Expression_and_Operator.cpp
+++ Chapter_3/Expression_and_Operator.cpp
@@ -3,9 +3,19 @@ using namespace std;
 
 int main()
 {
-    int number1, number2, result;
-    cout << "Press two numbers that you want to plus";
-    cin >> number1 >> number2;
-    result = number1 + number2;
-    cout << "Result of \"" << number1 << " + " << number2 << "\" is " << result << endl;
+    int value1;
+    char spc = ' '; // Decalre a valuable
+
+    cout << "Press three-digits number!! ";
+    cin >> value1; // Input three-digits number
+
+    cout << value1 << " is ";
+
+    cout << (value1 / 100) << spc;
+    value1 %= 100; // Output number that hundred's place number and delete it from valuable
+
+    cout << (value1 / 10) << spc;
+    value1 %= 10; // Output number that ten's place number and delete it from valuable
+
+    cout << value1 << endl; // Output last number
 }
