ded3b5660c927e7b25f04552f415a2925cef2451
Jeongmo <wjdah1402@gmail.com>
2023-01-18 14:44:36 +0900

    Program that sum all numbers between two numbers

--- Chapter_4/Control_flow_and_loop_statements.cpp
+++ Chapter_4/Control_flow_and_loop_statements.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int main()
 {
-    int number = 1, index = 1, multiple; // Declare valuable
-    while (number < 10) // Make rows
+    int value1, value2, result = 0; // Declare valuable
+    cout << "Enter two numbers that you want to know sum all number between these : "; // Input two numbers
+    cin >> value1 >> value2;
+
+    if (value1 > value2) // Revert if value1 is bigger than value2
     {
-        while(index < 10) // Make lines
-        {
-            multiple = number * index; // Multiplies
-            cout << number << "X" << index << "=" << ((multiple >= 10) ? "" : " ") << multiple << "\t";
-            // Output multiplies with conditonal operator
-            index += 1;
-        }
-        index = 1; // reset index
-        cout << endl;
-        number += 1;
+        result = value1;
+        value1 = value2;
+        value2 = result;
+        result = 0;
     }
+
+    for (int i = value1; !(i > value2); i++) // Plus all number between two numbers and save result
+    {
+        result += i;
+    }
+    cout << "Sum between " << value1 << " and " << value2 << " is " << result << endl;
 }
