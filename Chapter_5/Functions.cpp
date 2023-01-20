7ae264371ae198c1b1539998c25ce3d8c7c66abe
Jeongmo <wjdah1402@gmail.com>
2023-01-20 14:57:40 +0900

    Practice function using adress and pointer

--- Chapter_5/Functions.cpp
+++ Chapter_5/Functions.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int large_number(int value1, int value2)
+void Absolute_value(int &value)
 {
-    if (value1 < value2)
+    if (value >= 0)
     {
-        return value2;
+        value = ((value == 0) ? 0 : value);
     }
     else
     {
-        return value1;
+        value =((-1) * value);
+    }
+}
+
+void largest_number_under_hundred(int *value)
+{
+    for (int i = 2; *value < 100; i++)
+    {
+        if ((*value * i) < 100)
+        {
+            continue;
+        }
+        else
+        {
+            *value *= i-1;
+            break;
+        }
     }
 }
 
 int main()
 {
-    int num1, num2;
+    int num1;
 
-    cout << "Enter first number you want to compare : ";
+    cout << "Enter a number : ";
     cin >> num1;
-    cout << "Enter second number you want to compare : ";
-    cin >> num2;
 
-    cout << "The large number between two numbers is ";
-    cout << large_number(num1,num2) << endl;
+    Absolute_value(num1);
+    cout << "Absolute number of this number is " << num1 <<endl;
+
+    largest_number_under_hundred(&num1);
+    cout << "Largest number under a hundred that multiplication of number is : " << num1 << endl;
+
     return 0;
 }
