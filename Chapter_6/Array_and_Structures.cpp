fc04b18a8f8d42ab2231e33255c930b272d63cc8
Jeongmo <wjdah1402@gmail.com>
2023-01-23 16:03:04 +0900

    Practice global and local valuable

--- Chapter_6/Array_and_Structures.cpp
+++ Chapter_6/Array_and_Structures.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#define output(value) cout << "value = " << value << endl; // output function
 using namespace std;
+int value = 10; // Declare global value
+
+void Another_local_valuable() // For local valuable in other function
+{
+    int value = 40;
+    output(value);
+}
 
 int main()
 {
+    output(value); // output global valuable : ln 4
+    int value = 30;
+
+    {int value = 20; output(value);} // output local valuable : ln 17
+
+    output(value); // output local valuable : ln 15
+    Another_local_valuable(); // output local valuable : 8
     return 0;
 }
