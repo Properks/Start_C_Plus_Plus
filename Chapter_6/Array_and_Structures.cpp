5cb55d5a819d1f08aa23e700f71c5836f9e0f6f1
Jeongmo <wjdah1402@gmail.com>
2023-01-23 17:01:42 +0900

    Static valuable without array #18

--- Chapter_6/Array_and_Structures.cpp
+++ Chapter_6/Array_and_Structures.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
-#define output(value) cout << "value = " << value << endl; // output function
 using namespace std;
-int value = 10; // Declare global value
 
-void Another_local_valuable() // For local valuable in other function
+int Count() // count number with static valuable
 {
-    int value = 40;
-    output(value);
+    static int count = 0;
+    ++count;
+    return count;
+}
+
+void Enter_password(int &user)
+{
+    cout << "\nEnter your password (8 numbers) : ";
+    cin >> user;
 }
 
 int main()
 {
-    output(value); // output global valuable : ln 4
-    int value = 30;
-
-    {int value = 20; output(value);} // output local valuable : ln 17
-
-    output(value); // output local valuable : ln 15
-    Another_local_valuable(); // output local valuable : 8
+    int password = 20230123, user;
+    for (int i = 0; i < 100; i++)
+    {
+        Enter_password(user);
+        if (user == password)
+        {
+            cout << "Log-in Succeed" << endl;
+            break;
+        }
+        else
+        {
+            cout << "Incorrect " << Count() << " times" << endl;
+            continue;
+        }
+    }
     return 0;
 }
