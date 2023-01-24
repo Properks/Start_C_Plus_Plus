73f7f99f270a70f5340502d9e662fea756a1ce1d
Jeongmo <wjdah1402@gmail.com>
2023-01-24 12:08:16 +0900

    Pointer array Output type

--- Chapter_6/Array_and_Structures.cpp
+++ Chapter_6/Array_and_Structures.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int Compare(int *Array)
-{
-    int largest = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        if (largest < *(Array + i))
-        {
-            largest = *(Array + i);
-        }
-    }
-    return largest;
-}
-
 int main()
 {
-    int Array[10];
-    cout << "Enter 10 numbers : ";
-    for (int i = 0; i < 10; i++)
-    {
-        cin >> Array[i];
-    }
-    cout << "The largest number is " << Compare(Array) << endl;
+    char Array[10] = "computer";
+    char *str;
+    str = Array;
+
+    cout << str << endl; // ouput "computer" (adress)
+    cout << *str << endl; // output "c" (valuable)
     return 0;
 }
