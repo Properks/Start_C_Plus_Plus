f400562afab7797cb4280b84042d88b2fb1ff6dc
Jeongmo <wjdah1402@gmail.com>
2023-01-25 15:54:49 +0900

    Compare two string with include string

--- Chapter_6/Array_and_Structures.cpp
+++ Chapter_6/Array_and_Structures.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    char Array[10] = "computer";
-    char *str;
-    str = Array;
+    char str[5][11];
+    cout << "Enter 5 string" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i+1 << " string : ";
+        cin >> str[i];
+    }
 
-    cout << str << endl; // ouput "computer" (adress)
-    cout << *str << endl; // output "c" (valuable)
+    char *max = str[0];
+
+    for (int i = 1; i < 5; i++)
+    {
+        if ((strcmp(max, str[i]) < 0))
+        {
+            max = str[i];
+        }
+    }
+
+    cout << "The largest string is " << max << endl;
     return 0;
 }
