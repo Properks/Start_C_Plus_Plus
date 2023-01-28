20c770b8597d02bed63d3879535c0f90701c874c
Jeongmo <wjdah1402@gmail.com>
2023-01-28 15:34:27 +0900

    Practice about Structures

--- Chapter_6/Array_and_Structures.cpp
+++ Chapter_6/Array_and_Structures.cpp
@@ -1,31 +1,54 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
-int Find_count_of_character(char array[1024], char character) // Array and character were inputed
+struct Score
 {
-    int count = 0;
-    char *Save_string = array; // Declare pointer with string inputed
-    while (strchr(Save_string,character) != NULL) // If string doesn't have chracter, it will break
-    {
-        Save_string = strchr(Save_string, character); // Find character in string
-        Save_string = strchr(Save_string, *(Save_string+1)); // Remove first character in string in order not to do infinite loop
-        count++;
-    }
-    return count;
+    char name[16];
+    long Student_ID;
+    int Korea;
+    int Math;
+    int English;
+}value1, value2;
+
+void input()
+{
+    cout << "Enter student's name : ";
+    cin >> value1.name;
+    cout << "Enter student's ID : ";
+    cin >> value1.Student_ID;
+    cout << "Enter student's Korea score : ";
+    cin >> value1.Korea;
+    cout << "Enter student's Math score : ";
+    cin >> value1.Math;
+    cout << "Enter student's English score : ";
+    cin >> value1.English;
+
+    cout << "Enter student's name : ";
+    cin >> value2.name;
+    cout << "Enter student's ID : ";
+    cin >> value2.Student_ID;
+    cout << "Enter student's Korea score : ";
+    cin >> value2.Korea;
+    cout << "Enter student's Math score : ";
+    cin >> value2.Math;
+    cout << "Enter student's English score : ";
+    cin >> value2.English;
 }
 
 int main()
 {
-    char String[1024], Find_character;
+    input();
 
-    cout << "Enter a sentence in English (len = 1024) : ";
-    cin.getline(String,1024); // input with cin.getline in order to include space bar
-    cout << "Enter a character that you want to find : ";
-    cin >> Find_character;
-
-    cout << "Count of " << Find_character << " in \"" << String <<  "\" is " 
-    << Find_count_of_character(String, Find_character) << endl;
+    cout << "------------------------------------" << endl;
     
+    cout << value1.name << "(" << value1.Student_ID << ") : " << 
+    value1.Korea <<","<< value1.Math <<","<< value1.English << endl 
+    << "Average score : " << (value1.Korea + value1.Math + value1.English)/3 << endl;
+    
+    cout << value2.name << "(" << value2.Student_ID << ") : " << 
+    value2.Korea <<","<< value2.Math <<","<< value2.English << endl 
+    << "Average score : " << (value2.Korea + value2.Math + value2.English)/3 << endl;
     return 0;
 }
