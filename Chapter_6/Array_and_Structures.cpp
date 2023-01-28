c17f0eebcb5b752fdaef1447340ef06082d31fd7
Jeongmo <wjdah1402@gmail.com>
2023-01-28 15:47:27 +0900

    Practice previous assignment with stucture array

--- Chapter_6/Array_and_Structures.cpp
+++ Chapter_6/Array_and_Structures.cpp
@@ -10,31 +10,46 @@ struct Score
     int Korea;
     int Math;
     int English;
-}value1, value2;
+};
+
+struct Score Array[2];
 
 void input()
 {
-    cout << "Enter student's name : ";
-    cin >> value1.name;
-    cout << "Enter student's ID : ";
-    cin >> value1.Student_ID;
-    cout << "Enter student's Korea score : ";
-    cin >> value1.Korea;
-    cout << "Enter student's Math score : ";
-    cin >> value1.Math;
-    cout << "Enter student's English score : ";
-    cin >> value1.English;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Enter student's name : ";
+        cin >> Array[i].name;
+        cout << "Enter student's ID : ";
+        cin >> Array[i].Student_ID;
+        cout << "Enter student's Korea score : ";
+        cin >> Array[i].Korea;
+        cout << "Enter student's Math score : ";
+        cin >> Array[i].Math;
+        cout << "Enter student's English score : ";
+        cin >> Array[i].English;
+    }
+    // cout << "Enter student's name : ";
+    // cin >> Array[0].name;
+    // cout << "Enter student's ID : ";
+    // cin >> Array[0].Student_ID;
+    // cout << "Enter student's Korea score : ";
+    // cin >> Array[0].Korea;
+    // cout << "Enter student's Math score : ";
+    // cin >> Array[0].Math;
+    // cout << "Enter student's English score : ";
+    // cin >> Array[0].English;
 
-    cout << "Enter student's name : ";
-    cin >> value2.name;
-    cout << "Enter student's ID : ";
-    cin >> value2.Student_ID;
-    cout << "Enter student's Korea score : ";
-    cin >> value2.Korea;
-    cout << "Enter student's Math score : ";
-    cin >> value2.Math;
-    cout << "Enter student's English score : ";
-    cin >> value2.English;
+    // cout << "Enter student's name : ";
+    // cin >> Array[1].name;
+    // cout << "Enter student's ID : ";
+    // cin >> Array[1].Student_ID;
+    // cout << "Enter student's Korea score : ";
+    // cin >> Array[1].Korea;
+    // cout << "Enter student's Math score : ";
+    // cin >> Array[1].Math;
+    // cout << "Enter student's English score : ";
+    // cin >> Array[1].English;
 }
 
 int main()
@@ -42,13 +57,19 @@ int main()
     input();
 
     cout << "------------------------------------" << endl;
+
+    for (int i = 0; i < 2; i++)
+    {
+        cout << Array[i].name << "(" << Array[i].Student_ID << ") : " << 
+        Array[i].Korea <<","<< Array[i].Math <<","<< Array[i].English << endl 
+        << "Average score : " << (Array[i].Korea + Array[i].Math + Array[i].English)/3 << endl;
+    }
+    // cout << Array[0].name << "(" << Array[0].Student_ID << ") : " << 
+    // Array[0].Korea <<","<< Array[0].Math <<","<< Array[0].English << endl 
+    // << "Average score : " << (Array[0].Korea + Array[0].Math + Array[0].English)/3 << endl;
     
-    cout << value1.name << "(" << value1.Student_ID << ") : " << 
-    value1.Korea <<","<< value1.Math <<","<< value1.English << endl 
-    << "Average score : " << (value1.Korea + value1.Math + value1.English)/3 << endl;
-    
-    cout << value2.name << "(" << value2.Student_ID << ") : " << 
-    value2.Korea <<","<< value2.Math <<","<< value2.English << endl 
-    << "Average score : " << (value2.Korea + value2.Math + value2.English)/3 << endl;
+    // cout << Array[1].name << "(" << Array[1].Student_ID << ") : " << 
+    // Array[1].Korea <<","<< Array[1].Math <<","<< Array[1].English << endl 
+    // << "Average score : " << (Array[1].Korea + Array[1].Math + Array[1].English)/3 << endl;
     return 0;
 }
