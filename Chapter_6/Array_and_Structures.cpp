4d06c236a6e3c77c02002a8b1e8d3007ef5d74f5
Jeongmo <wjdah1402@gmail.com>
2023-01-29 15:26:15 +0900

    Create a new memory and delete memory

--- Chapter_6/Array_and_Structures.cpp
+++ Chapter_6/Array_and_Structures.cpp
@@ -2,46 +2,17 @@
 #include <string>
 using namespace std;
 
-struct Data // Declare nested structure
-{
-    char name[16];
-    long Student_ID;
-    struct Score
-    {
-        int Korea;
-        int Math;
-        int English;
-    }score;
-};
-void input(struct Data *d) // Inputed with structure pointer
-{
-    for (int i = 0; i < 2; i++)
-    {
-        cout << "Enter student's name : ";
-        cin >> (d+i) -> name;   // (Address form of pointer) -> valuable in structure
-        cout << "Enter student's ID : ";
-        cin >> (d+i) -> Student_ID;
-        cout << "Enter student's Korea score : ";
-        cin >> (d+i) -> score.Korea;
-        cout << "Enter student's Math score : ";
-        cin >> (d+i) -> score.Math;
-        cout << "Enter student's English score : ";
-        cin >> (d+i) -> score.English;
-    }
-}
-
 int main()
 {
-    struct Data Array[2]; // Two student
-    input(Array); // Input address of Array[2]
+    char String[1024], *Memory; // Declare array and pointer
 
-    cout << "------------------------------------" << endl;
+    cout << "Enter a sentence : ";
+    cin.getline(String,1024); // input sentence
 
-    for (int i = 0; i < 2; i++) // output
-    {
-        cout << Array[i].name << "(" << Array[i].Student_ID << ") : " << 
-        Array[i].score.Korea <<","<< Array[i].score.Math <<","<< Array[i].score.English << endl 
-        << "Average score : " << (Array[i].score.Korea + Array[i].score.Math + Array[i].score.English)/3 << endl;
-    }
+    Memory = new char(strlen(String) + 1); // create new memory
+    strcpy(Memory, String);
+
+    cout << "\"" << Memory << "\" consists of " << strlen(Memory) << " letters" << endl;
+    delete [] Memory; // delete new memory
     return 0;
 }
