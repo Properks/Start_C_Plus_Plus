b96db75af30fd60bde89971f4546193b1acf4626
Jeongmo <wjdah1402@gmail.com>
2023-01-31 14:47:42 +0900

    Practice class with calculate lenth and square of circle

--- Chapter_7/Class_and_Object.cpp
+++ Chapter_7/Class_and_Object.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+class Circle
+{
+private:
+    double Pi;
+public:
+    double lenth(double radius)
+    {
+        Pi = 3.14;
+        return 2 * radius * Pi;
+    }
+    double square(double radius)
+    {
+        Pi = 3.14;
+        return pow(radius, 2) * Pi;
+    }
+}fomula;
 
 int main()
 {
+    double radius;
+    cout << "Enter a radius : ";
+    cin >> radius;
+
+    cout << "Lenth of circle have " << radius << " radius is "<< fomula.lenth(radius) << endl;
+    cout << "Square of circle have " << radius << " radius is "<< fomula.square(radius) << endl;
+
     return 0;
 }
