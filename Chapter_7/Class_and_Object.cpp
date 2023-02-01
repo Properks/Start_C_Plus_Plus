efac5f84515efdc05351f81d30d3622b853600a6
Jeongmo <wjdah1402@gmail.com>
2023-02-01 14:29:46 +0900

    Practice class function with return and inner functions

--- Chapter_7/Class_and_Object.cpp
+++ Chapter_7/Class_and_Object.cpp
@@ -2,74 +2,46 @@
 #include <string>
 using namespace std;
 
-class Privacy_Data
+class Sum_5_numbers
 {
-private:  // Declare class valuable
-    int age;
-    char *name;
-    char *ID;
-    char *Password;
+private:
+    int sum;
+    int numbers[5];
 public:
-    void Person_name(char *string) // Save inputed string in name
+    void Get_numbers()
     {
-        int len = strlen(string) + 1;
-        name = new char [len];
-        strcpy(name, string);
+        int data;
+        cout << "Enter 5 numbers : ";
+        for (int i = 0; i < 5; i++)
+        {
+            cin >> data;
+            numbers[i] = data;
+        }
     }
-    void Person_ID(char *string) // Save inputed string in ID
+    int Sum_numbers()
     {
-        int len = strlen(string) + 1;
-        ID = new char [len];
-        strcpy(ID, string);
+        sum = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum;
     }
-    void Person_Password(char *string) // Save inputed string in Password
+    void output()
     {
-        int len = strlen(string) + 1;
-        Password = new char [len];
-        strcpy(Password, string);
+        cout << "The sum of ";
+        for (int i = 0; i < 4; i++)
+        {
+            cout << numbers[i] << ", ";
+        }
+        cout << numbers[4] << " is " << Sum_numbers() << endl;
     }
-    void Person_age(int Age) // Save inputed number in name
-    {
-        age = Age;
-    }
-    void output() // output Data
-    {
-        cout << name << endl;
-        cout << ID << endl;
-        cout << Password << endl;
-        // delete [] name;      If you write like this and this project is bigger than now,
-        // delete [] ID;        No data in (Man1).name, (Man1).ID, (Man1).Password
-        // delete [] Password;
-    }
-}input, Man1, Man2, Man3;
+};
 
 int main()
 {
-    char String[1024]; // I will input string into only this array
-    int age;
-
-    cout << "Enter your age : ";
-    cin >> age;
-    if (age < 14) // If age is under 14, Can't continue
-    {
-        cout << "Sorry, You can't sign up, because your age is under 14" << endl;
-        return 0; // escape main function
-    }
-    input.Person_age(age);
-    cout << "Enter your name : ";
-    cin >> String;
-    input.Person_name(String);
-       
-    cout << "Enter your ID : ";
-    cin >> String;
-    input.Person_ID(String);
-
-    cout << "Enter your Password : ";
-    cin >> String;
-    input.Person_Password(String);
-
-    Man1 = input; // Copy class valuable(input) to Man1
-    // Man1.output();
-
+    Sum_5_numbers test;
+    test.Get_numbers();
+    test.output();
     return 0;
 }
