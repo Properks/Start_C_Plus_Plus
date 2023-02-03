efafe2a824be8ce1460605260ad29495851c4b82
Jeongmo <wjdah1402@gmail.com>
2023-02-03 14:34:40 +0900

    Simplify previous assignment and practice Class array

--- Chapter_7/Class_and_Object.cpp
+++ Chapter_7/Class_and_Object.cpp
@@ -5,33 +5,16 @@ using namespace std;
 class Sum_numbers
 {
 private:
-    int sum, *numbersPTR; // Declare sum of numbers valuable, and int pointer in order to use like array.
-    int count_of_numbers; // Count of numbers value.
-    static int count; // It is good at counting how many object is.
+    int sum, count_of_numbers, *numbersPTR; // Declare sum of numbers variable, and int pointer in order to use like array.
 public:
-    Sum_numbers(int value1) // Constructor for caculate and initialize valuable with member functions.
+    Sum_numbers() // Constructor for caculate and initialize variable with member functions.
     {
-        count++; // count constructor
         sum = 0;
-        Get_numbers(value1);
-        Sum();
-        output();
     }
-    Sum_numbers(const Sum_numbers &V1)
-    {
-        count++; // count copy constructor
-        numbersPTR = V1.numbersPTR;
-        count_of_numbers = V1.count_of_numbers;
-        sum = V1.sum;
-    }
-    ~Sum_numbers() // Destructor for deleting int pointer memory.
-    {
-        // delete [] numbersPTR;
-        cout << "The count of object is " << count << endl;
-    }
+    ~Sum_numbers() {} // Destructor for deleting variable memory.
     void Get_numbers(int num) // Input numbers and allocate memory.
     {
-        int data; // Valuable for input.
+        int data; // Variable for input.
         count_of_numbers = num;
         numbersPTR = new int [num];
         cout << "Enter " << num << " numbers : ";
@@ -57,17 +40,20 @@ public:
             cout << *(numbersPTR+i) << ", ";
         }
         cout << *(numbersPTR+count_of_numbers - 1) << " is " << sum << endl;
+        delete [] numbersPTR; // In order not to create memory leak
         // If you write function has return in output(cout or function for output), function is executed whenever output
     }
 };
-int Sum_numbers::count = 0; // Static member valuable can initialize outside the class
+
 int main()
 {
     int num;
     cout << "Enter how many numbers that you want to enter? : "; // Input count of numbers
     cin >> num;
-    Sum_numbers test(num); // Declare constructor.
-    Sum_numbers Copy_test(test);
-    Copy_test.output();
+    Sum_numbers test[2]; // Declare constructor array.
+    test[0].Get_numbers(num);
+    test[0].Sum();
+    test[1] = test[0];
+    test[1].output();
     return 0;
 }
