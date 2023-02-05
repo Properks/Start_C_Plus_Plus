b65cd7d400bc3a815daa96f39a7ae97340c56b0f
Jeongmo <wjdah1402@gmail.com>
2023-02-05 19:11:10 +0900

    Practice about reference declarator

--- Chapter_8/Operator_overloading.cpp
+++ Chapter_8/Operator_overloading.cpp
@@ -6,33 +6,51 @@ class Sum_another_object
 private:
     int x;
 public:
-    Sum_another_object();
-    Sum_another_object(int value1);
+    Sum_another_object(); // Declare constructor.
+    Sum_another_object(int value1); 
     ~Sum_another_object() {}
-    int operator + (const Sum_another_object &A)
+    // Sum_another_object operator + (int value) // Declare operator overloading with variable.
+    // {
+    //     this -> x = x + value;
+    //     return *this;
+    // }
+    // Sum_another_object operator + (const Sum_another_object &A) // Declare operator overloading with another object.
+    // {
+    //     Sum_another_object AB = this -> x + A.x;
+    //     return AB;
+    // }
+    void output() // output member function for reaching private variable
     {
-        int sum = x + A.x;
-        return sum;
+        cout << "X is " << x << endl;
+    }
+    void Plus(int value) // Sum member function for reaching private variable
+    {
+        this -> x += value;
     }
 };
 
-Sum_another_object::Sum_another_object()
+Sum_another_object::Sum_another_object() // initialize member variable
 {
     x = 0;
 }
 
-Sum_another_object::Sum_another_object(int value1)
+Sum_another_object::Sum_another_object(int value1) 
 {
     x = value1;
 }
 
 int main()
 {
-    int sum;
-    Sum_another_object A1(3);
-    Sum_another_object A2(7);
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
 
-    sum = A1 + A2;
-    cout << "Sum is : " << sum << endl;
+    Sum_another_object A1;
+    
+    Sum_another_object& A3 = A1; // Practice reference declarator(&)
+                                // If A1's member variables are changed, A3's member variables are also changed.
+    A1.Plus(num); // change 'A1.x' to value that plus 'num'
+
+    A3.output(); // Outputed result is the added value
     return 0;
 }
