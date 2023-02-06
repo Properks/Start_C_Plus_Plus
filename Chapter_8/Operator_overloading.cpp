37d467afd4cdee5ebcf07ed87604a4cfaf282a6b
Jeongmo <wjdah1402@gmail.com>
2023-02-06 14:25:17 +0900

    Prefix and Postfix, increment and decrement operator overloading

--- Chapter_8/Operator_overloading.cpp
+++ Chapter_8/Operator_overloading.cpp
@@ -14,13 +14,15 @@ public:
         this -> x = x + value;
         return *this;
     }
-    Sum_another_object operator + (Sum_another_object &A1) // Declare operator overloading with another object.
+    Sum_another_object& operator ++(int) // postfix increment operator overloading
     {
-        Sum_another_object AB = this -> x + A1.x;
-        A1.x += 1; // modify value of the A1.x
-                    // 'A1.x += 1' doesn't affect at 'AB = this -> x + A1.x'. but, A1.x will change after function
-        AB = this -> x + A1.x; // this -> x = 1, A1.x = 1(not 2). result is 2
-        return AB;
+        this -> x += 1;
+        return *this;
+    }
+    Sum_another_object& operator --() // prefix decrement operator overloading
+    {
+        this -> x -= 1;
+        return *this;
     }
     void output() // output member function for reaching private variable
     {
@@ -45,19 +47,11 @@ Sum_another_object::Sum_another_object(int value1)
 int main()
 {
     Sum_another_object A1(1);
-    Sum_another_object A2;
 
-    cout << "First value of A2.x : "; // print first A2.x
-    A2.output();
-
-    A2 = A2 + A1; 
-
-    cout << "A2.x : ";
-    A2.output();  // output 2
-
-    cout << "A1.x : ";
-    A1.output(); // output 2, if you write 'Sum_another_object &A1' in [Ln17]
-                // If you write 'Sum_another_object A1', this print 1. because A1.x didn't change
+    A1++; // postfix
+    A1.output();
+    --A1; // prefix
+    A1.output();
 
     return 0;
 }
