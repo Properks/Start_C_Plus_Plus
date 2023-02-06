20bd817ed7bdd18a0f96b87962447cb8ca356db3
Jeongmo <wjdah1402@gmail.com>
2023-02-06 14:41:20 +0900

    Relation operator overloading

--- Chapter_8/Operator_overloading.cpp
+++ Chapter_8/Operator_overloading.cpp
@@ -4,25 +4,18 @@ using namespace std;
 class Sum_another_object
 {
 private:
-    int x;
+    int x,y;
 public:
     Sum_another_object(); // Declare constructor.
-    Sum_another_object(int value1); 
+    Sum_another_object(int value1, int value2); 
     ~Sum_another_object() {}
-    Sum_another_object operator + (int value) // Declare operator overloading with variable.
+    int operator && (const Sum_another_object &A) // Check all member variables of the (this) and A have value.
     {
-        this -> x = x + value;
-        return *this;
+        return (this -> x && A.x) && (this -> y && A.y);
     }
-    Sum_another_object& operator ++(int) // postfix increment operator overloading
+    int operator == (const Sum_another_object &A) // Check (this) is same as A.
     {
-        this -> x += 1;
-        return *this;
-    }
-    Sum_another_object& operator --() // prefix decrement operator overloading
-    {
-        this -> x -= 1;
-        return *this;
+        return (this -> x == A.x) && (this -> y == A.y);
     }
     void output() // output member function for reaching private variable
     {
@@ -37,21 +30,27 @@ public:
 Sum_another_object::Sum_another_object() // initialize member variable
 {
     x = 0;
+    y = 0;
 }
 
-Sum_another_object::Sum_another_object(int value1) 
+Sum_another_object::Sum_another_object(int value1, int value2) 
 {
     x = value1;
+    y = value2;
+
 }
 
 int main()
 {
-    Sum_another_object A1(1);
+    Sum_another_object A1(1,3);
+    Sum_another_object A2(2,4);
+    Sum_another_object A3(3,0);
 
-    A1++; // postfix
-    A1.output();
-    --A1; // prefix
-    A1.output();
+    if (A1 && A2) {cout << "All member variables of the A1 and A2 have value."  << endl;}
+    else{cout << "Not at all member variables of the A1 and A2 have value."  << endl;}
 
+    if (A1 == A2){cout << "A1 is the same as A2" << endl;}
+    else{cout << "A1 and A2 are different" << endl;}
+    
     return 0;
 }
