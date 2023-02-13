30391faf6fa3da8414cb85d7ac1c7cd0689d3614
Jeongmo <wjdah1402@gmail.com>
2023-02-13 15:06:21 +0900

    Combine two strings with operator overloading '+='

--- Chapter_8/Operator_overloading.cpp
+++ Chapter_8/Operator_overloading.cpp
@@ -11,15 +11,8 @@ public:
     Sum_String(); // For operator +
     Sum_String(const char *str); // initialize string
     ~Sum_String();
-    Sum_String operator +(const Sum_String &A) // Combine two strings
-    {
-        Sum_String Added;
-        Added.len = strlen(this -> StrArr) + strlen(A.StrArr);
-        Added.StrArr = new char [Added.len + 1];
-        strcpy(Added.StrArr, this -> StrArr); // copy Added.StrArr
-        strcat(Added.StrArr, A.StrArr); // Added A.StrArr
-        return Added;
-    }
+    Sum_String operator +(const Sum_String &A); // Combine two strings '+'
+    Sum_String& operator +=(const Sum_String &A); // Combine two strings '+='
     friend ostream& operator << (ostream &os, Sum_String &object) // print string
     {
         os << object.StrArr;
@@ -41,19 +34,38 @@ Sum_String::~Sum_String()
 {
     delete [] StrArr;
 }
+Sum_String Sum_String::operator +(const Sum_String &A) // Combine two strings '+'
+{
+    Sum_String Added;
+    Added.len = strlen(this -> StrArr) + strlen(A.StrArr);
+    Added.StrArr = new char [Added.len + 1];
+    strcpy(Added.StrArr, this -> StrArr); // copy Added.StrArr
+    strcat(Added.StrArr, A.StrArr); // Added A.StrArr
+    return Added;
+}
+Sum_String& Sum_String::operator +=(const Sum_String &A)
+{
+    char temp[512];
+    strcpy(temp, this -> StrArr);
+    this -> len += A.len;
+    this -> StrArr = new char [this -> len + 1];
+    strcpy(this -> StrArr, temp);
+    strcat(this -> StrArr, A.StrArr);
+    return *this;
+}
 int main()
 {
-    char STRARR[100];
+    char STRARR[512];
 
     cout << "Enter first string : "; // Input first string and save in First_string object
-    cin.getline(STRARR, 100);
+    cin.getline(STRARR, 512);
     Sum_String First_string(STRARR);
 
     cout << "Enter second string : "; // Input second string and save in Second_string object
-    cin.getline(STRARR, 100);
+    cin.getline(STRARR, 512);
     Sum_String Second_string(STRARR);
 
-    Sum_String Third_string = First_string + Second_string; // Using '+' operator overloading
-    cout << Third_string << endl;
+    First_string += Second_string; // Using '+' operator overloading
+    cout << First_string << endl;
     return 0;
 }
