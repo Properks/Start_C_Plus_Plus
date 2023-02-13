d90d0e418b03e67c90c2ac9a83e5fe5bfeae734c
Jeongmo <wjdah1402@gmail.com>
2023-02-13 15:48:34 +0900

    Combine two strings with operator overloading '>>'

--- Chapter_8/Operator_overloading.cpp
+++ Chapter_8/Operator_overloading.cpp
@@ -8,27 +8,30 @@ private:
     char *StrArr;
     int len;
 public:
-    Sum_String(); // For operator +
-    Sum_String(const char *str); // initialize string
+    Sum_String(); // Constructor
     ~Sum_String();
     Sum_String operator +(const Sum_String &A); // Combine two strings '+'
     Sum_String& operator +=(const Sum_String &A); // Combine two strings '+='
+    friend istream& operator >> (istream &is, Sum_String &object) // Input string
+    {
+        char temp[512];
+        is.getline(temp, 512);
+        object.len = strlen(temp);
+        object.StrArr = new char [object.len + 1];
+        strcpy(object.StrArr, temp);
+        return is;
+    }
     friend ostream& operator << (ostream &os, Sum_String &object) // print string
     {
         os << object.StrArr;
         return os;
     }
 };
+
 Sum_String::Sum_String()
 {
     StrArr = NULL;
     len = 0;
-}
-Sum_String::Sum_String(const char *str)
-{
-    len = strlen(str);
-    StrArr = new char [len + 1];
-    strcpy(StrArr, str);
 }
 Sum_String::~Sum_String()
 {
@@ -55,17 +58,20 @@ Sum_String& Sum_String::operator +=(const Sum_String &A)
 }
 int main()
 {
-    char STRARR[512];
+    Sum_String First_string;
+    cout << "Enter First string : "; // Input first string and save in First_string object
+    cin >> First_string;
 
-    cout << "Enter first string : "; // Input first string and save in First_string object
-    cin.getline(STRARR, 512);
-    Sum_String First_string(STRARR);
+    Sum_String Second_string;
+    cout << "Enter Second string : "; // Input second string and save in Second_string object
+    cin >> Second_string;
 
-    cout << "Enter second string : "; // Input second string and save in Second_string object
-    cin.getline(STRARR, 512);
-    Sum_String Second_string(STRARR);
+    Sum_String Third_string;
+    cout << "Enter Third string : "; // Input second string and save in Third_string object
+    cin >> Third_string;
 
-    First_string += Second_string; // Using '+' operator overloading
-    cout << First_string << endl;
+    Second_string += Third_string; // Using '+=' operator overloading
+    Sum_String Combined_string = First_string + Second_string;
+    cout << Combined_string << endl;
     return 0;
 }
