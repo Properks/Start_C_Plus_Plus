bdc231f0b7df3711ba962b933107652a431a80f3
Jeongmo <wjdah1402@gmail.com>
2023-02-16 14:10:44 +0900

    Check the order in which constructor and destructor runs

--- Chapter_9/Inheritance.cpp
+++ Chapter_9/Inheritance.cpp
@@ -5,13 +5,25 @@ class base
 private:
     int value1, value2;
 public:
+    base(); // Derived constructor require default constructor of base class
+    ~base();
     int Sum_two_values();
-    friend istream& operator >> (istream &is, base &ob)
+    friend istream& operator >> (istream &is, base &ob) // '>>' operator overloading
     {
         is >> ob.value1 >> ob.value2;
         return is;
     }
 };
+base::base()
+{
+    value1 = 0;
+    value2 = 0;
+    cout << "Base constructor" << endl; // Checking the order in which it runs.
+}
+base::~base()
+{
+    cout << "Base destructor" << endl; // Checking the order in which it runs.
+}
 
 int base::Sum_two_values()
 {
@@ -23,15 +35,15 @@ class Derived : private base
 private:
     int k;
 public:
-    Derived(int i) {k = i;}
-    ~Derived() {}
+    Derived(int i) {k = i; cout << "Derived constructor" << endl;} // Checking the order in which it runs.
+    ~Derived() {cout << "Derived destructor" << endl;} // Checking the order in which it runs.
     int Multiply()
     {
         return Sum_two_values() * k;
     }
-    void input()
+    void input() // use function because of private derived
     {
-        cin >> *this;
+        cin >> *this; // Call '>>' operator overloading in base class
     }
 };
 
