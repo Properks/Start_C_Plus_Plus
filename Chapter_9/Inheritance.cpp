419e329203fb1952d9a35d31039f442983f5c1b3
Jeongmo <wjdah1402@gmail.com>
2023-02-16 15:46:56 +0900

    Implement  Multi-level inheritance in designing game

--- Chapter_9/Inheritance.cpp
+++ Chapter_9/Inheritance.cpp
@@ -1,59 +1,101 @@
 #include <iostream>
+#include<string>
 using namespace std;
-class base
+// Design a class hierarchy for the characters in the game
+class Charater // Including Health, mana point and defense(ex. resistance) 
 {
 private:
-    int value1, value2;
+    int Max_Health_point;
+    int Max_Mana_point;
 public:
-    base(); // Derived constructor require default constructor of base class
-    ~base();
-    int Sum_two_values();
-    friend istream& operator >> (istream &is, base &ob) // '>>' operator overloading
-    {
-        is >> ob.value1 >> ob.value2;
-        return is;
-    }
-};
-base::base()
+    int Current_Health_point;
+    int Current_Mana_point;
+    int Magic_resistance;
+    void Set_point(int health, int man);
+}; 
+void Charater::Set_point(int health, int mana) 
 {
-    value1 = 0;
-    value2 = 0;
-    cout << "Base constructor" << endl; // Checking the order in which it runs.
-}
-base::~base()
-{
-    cout << "Base destructor" << endl; // Checking the order in which it runs.
+    Max_Health_point = health;
+    Max_Mana_point = mana;
+    Current_Health_point = Max_Health_point;
+    Current_Mana_point = Max_Mana_point;
 }
 
-int base::Sum_two_values()
-{
-    return value1 + value2;
-}
-
-class Derived : private base
+class Warrior : public Charater // Warrior character including normal attack power and special attack
 {
 private:
-    int k;
+    int Weapon_strength;
+    int Special_attacks;
 public:
-    Derived(int i) {k = i; cout << "Derived constructor" << endl;} // Checking the order in which it runs.
-    ~Derived() {cout << "Derived destructor" << endl;} // Checking the order in which it runs.
-    int Multiply()
+    Warrior();
+    ~Warrior() {}
+    void Weapon_str(Charater &A) // Normal attack
     {
-        return Sum_two_values() * k;
+        A.Current_Health_point -= Weapon_strength;
     }
-    void input() // use function because of private derived
+    void Special_atk(Charater &A) // Special attack
     {
-        cin >> *this; // Call '>>' operator overloading in base class
+        if (this -> Current_Mana_point < 2)
+        {
+            cout << "Cannot attack because of no mana" << endl;
+            return;
+        }
+        else
+        {
+            A.Current_Health_point -= (Weapon_strength * Special_attacks - A.Magic_resistance); // Calculate resistance
+            this -> Current_Mana_point -=  Special_attacks; // reduce mana
+        }
     }
 };
+
+Warrior::Warrior() // Initialize stats of warrior
+{
+    Set_point(100, 20);
+    Weapon_strength = 8;
+    Special_attacks = 2;
+    Magic_resistance = 0;
+}
+
+class Mage : public Charater
+{
+private:
+    int Spell_power;
+public:
+    Mage();
+    ~Mage() {}
+    void Spell_pw(Charater &A) // Spell attack
+    {
+        if (this -> Current_Mana_point < 3)
+        {
+            cout << "Cannot attack because of no mana" << endl;
+            return;
+        }
+        else
+        {
+            A.Current_Health_point -= (Spell_power * 5 - A.Magic_resistance);
+            this -> Current_Mana_point -=  Spell_power;
+        }
+    }
+};
+
+Mage::Mage() // initialize stats of mage
+{
+    Set_point(70, 50);
+    Spell_power = 3;
+    Magic_resistance = 3;
+}
+void print(Mage A, Warrior B) // For interface
+{
+    cout << endl << "Warrior" << endl;
+    cout << "Health" << B.Current_Health_point << "/" << 100 << endl;
+    cout << "Mana" << B.Current_Mana_point << "/" << 20 << endl;
+    cout << "Mage" << endl;
+    cout << "Health" << A.Current_Health_point << "/" << 70 << endl;
+    cout << "Mana" << A.Current_Mana_point << "/" << 50 << endl << endl;
+}
 
 int main()
 {
-    int num1, num2;
-    Derived ob(3);
-
-    cout << "Enter two numbers : ";
-    ob.input();
-    cout << ob.Multiply() << endl;
+    
     return 0;
 }
