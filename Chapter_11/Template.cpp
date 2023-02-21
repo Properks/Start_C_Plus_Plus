238da968b8b291d2d84e6c029dd0a0043100c5f3
Jeongmo <wjdah1402@gmail.com>
2023-02-21 14:51:06 +0900

    Template overload

--- Chapter_11/Template.cpp
+++ Chapter_11/Template.cpp
@@ -15,19 +15,34 @@ template <class T> T Calculator(T n, T m, char C) // Template in order to calcul
     }
     assert(false); // If it have error, print error
 } //FIXME: Input template form. I don't know User input which form.
+template <class Type> Type sum(Type n, Type m)
+{
+    return (n+m);
+}
+template <class Type> Type sum(Type *Array, int A)
+{
+    Type total = 0;
+    for (int i = 0; i < A; i++)
+    {
+        total += *(Array + i);
+    }
+    return total;
+}
 
 int main()
 {
-    char form;
     int A, B;
-    double D1, D2;
-    cout << "Enter a form will calculate(int) -> "; // Input two numbers and operator
-    cin >> A >> form >> B;
-    cout << "= " << Calculator(A,B,form) << endl;
+    int arr[10];
+    cout << "Enter two numbers -> "; // Input two numbers
+    cin >> A >> B;
+    cout << "= " << sum(A,B) << endl;
 
-    cout << "Enter a form will calculate(double) -> "; // Input two numbers and operator
-    cin >> D1 >> form >> D2;
-    cout << "= " << Calculator(D1,D2,form) << endl;
+    cout << "Enter two numbers -> "; // Input 10 numbers
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "= " << sum(arr,10) << endl;
 
     return 0;
 }
