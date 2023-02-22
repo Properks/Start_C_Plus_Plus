bc6f3d4e67fcd12edd18c75e0759e7a02b1296ef
Jeongmo <wjdah1402@gmail.com>
2023-02-22 15:28:33 +0900

    Checking user enter something correctly or not

--- Chapter_12/Input_Output.cpp
+++ Chapter_12/Input_Output.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() // Enter specific input to escape.
 {
+    int value;
+    while (true)
+    {
+        cout << "Enter a number to escape : ";
+        cin >> value;
+        if (cin.fail() == 1) // If you don't enter number, this will work.
+        {
+            cin.clear(); // clear error flag and allow the program to continue reading input from `cin`.
+            cin.ignore(100, '\n'); // prevent any remaining characters in the input buffer from causing further input issues.
+
+            cout << "This isn't a number" << endl;
+        }
+        else
+        {
+            cout << "This is a number" << endl;
+            break; // escape from while statement.
+        }
+    }
     return 0;
 }
