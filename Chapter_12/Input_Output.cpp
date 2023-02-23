0fb4d26b0ddfadc60f466c1429fc84e6884005b2
Jeongmo <wjdah1402@gmail.com>
2023-02-23 12:57:31 +0900

    How to use 'cout.put','cout.write'

--- Chapter_12/Input_Output.cpp
+++ Chapter_12/Input_Output.cpp
@@ -6,16 +6,12 @@ int main() // Enter specific input to escape.
     char *PTRstr;
     char ch;
     PTRstr = new char [100];
-    // `cin >> ch;` ignore 'space', but `ch = cin.get();` doesn't ignore 'space'
-    cout << "Enter string with space : "; // Suppose to enter "Nick, Amy"
-    cin.getline(PTRstr, 100, ','); // 'PTRstr' is 'Nick' and cursor behind ','.
+    
+    cout << "Enter string with space : "; 
+    cin.getline(PTRstr, 100); 
 
-    cin >> ch; // If I use `ch = cin.get();` first, 'ch' will be ' '(space).
-    cout << "cin >> ch : " << ch << endl; // output is A (because `cin >> ch` ignore 'space').
-
-    ch = cin.get(); // If I use `ch = cin.get();` first, 'ch' will be 'A'.
-    cout << "cin.get() : " << ch << endl; // Output is 'm'.
-
+    cout.write(PTRstr,5); // print string of lenth 5
+    cout.put(PTRstr[5]) << endl; // print character
     delete [] PTRstr;
     return 0;
 }
