fe0d0e36d1f8b4c89803411238b1ab53bdca8868
Jeongmo <wjdah1402@gmail.com>
2023-02-23 13:52:39 +0900

    Set how many print decimal point with 'cout.precision()' and 'cout << setprecision()'

--- Chapter_12/Input_Output.cpp
+++ Chapter_12/Input_Output.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() // Enter specific input to escape.
 {
-    int Width;
-    cout << " Enter width : ";
-    cin >> Width;
-    for (int i = 1; i < Width + 1; i++)
-    {
-        cout.width(Width - (i-1)); // Set width
-        cout.fill('0'); // Fill character as much as width. If width is 5, print 4 character and cursor on 5th.
-        for (int j = 0; j < i; j++)
-        {
-            cout << 8;
-        }
-        cout << endl;
-    }
+    double Pi = 3.1415926535897932384626433832;
+    cout << "Using 'cout << setprecision(3) in iomanip : " << setprecision(3) << Pi << endl; // 3.14
+    cout << "Using 'cout << setprecision(4) in iomanip : " << setprecision(4) << Pi << endl; // 3.142 (ROUND OFF)
+    cout << "Using 'cout << setprecision(6) in iomanip : " << setprecision(6) << Pi << endl; // 3.14159
+        // 'cout << setprecision' and 'cout.precision' set the number of digits before and after the decimal point
+    cout << endl;
+
+    double num1 = 123.4536;
+    cout.precision(3);
+    cout << "Using 'cout.precision(3)' in iostream : " << num1 << endl; // 123
+    cout.precision(4);
+    cout << "Using 'cout.precision(4)' in iostream : " << num1 << endl; // 123.5 (ROUND OFF)
+    cout.precision(6);
+    cout << "Using 'cout.precision(6)' in iostream : " << num1 << endl; // 123.454 (ROUND OFF)
+
+    // Difference between 'cout << setprecision' and 'cout.precision' is library(<iostream>, <iomanip>)
     return 0;
 }
