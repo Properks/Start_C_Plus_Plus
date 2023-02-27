413728a3e38073e90539a418f508e51bdb723a4f
Jeongmo <wjdah1402@gmail.com>
2023-02-27 15:06:23 +0900

    Add exception of char type

--- Chapter_13/Exception_Handling.cpp
+++ Chapter_13/Exception_Handling.cpp
@@ -8,6 +8,10 @@ int main()
     cin >> n;
     try
     {
+        if (cin.fail() == 1)
+        {
+            throw 'A';
+        }
         if (n < 1 || n > 9)
         {
             throw n;
@@ -16,6 +20,11 @@ int main()
     catch(int non_positive)
     {
         cout << "Error!! " << non_positive << " isn't positive number under 10" << endl;
+        return 0;
+    }
+    catch(char ch)
+    {
+        cout << "Error!! This isn't a number" << endl;
         return 0;
     }
     cout << n << " is a positive number under 10" << endl;
