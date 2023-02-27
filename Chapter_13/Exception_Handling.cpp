1ca14f025127c7a1436b879dcd78b0b7eea3d74a
Jeongmo <wjdah1402@gmail.com>
2023-02-27 14:50:49 +0900

    Try and throw and catch

--- Chapter_13/Exception_Handling.cpp
+++ Chapter_13/Exception_Handling.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cout << "Enter a positive number under 10 : ";
+    cin >> n;
+    try
+    {
+        if (n < 1 || n > 9)
+        {
+            throw n;
+        }
+    }
+    catch(int non_positive)
+    {
+        cout << "Error!! " << non_positive << " isn't positive number under 10" << endl;
+        return 0;
+    }
+    cout << n << " is a positive number under 10" << endl;
     return 0;
 }
