db225e3fcece7c061263d978ac512ceab29f3140
Jeongmo <wjdah1402@gmail.com>
2023-02-27 15:25:03 +0900

    Add exception of char type with function

--- Chapter_13/Exception_Handling.cpp
+++ Chapter_13/Exception_Handling.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void checking(int n)
+{
+    if (cin.fail() == 1)
+    {
+        throw 'A';
+    }
+    if (n < 1 || n > 9)
+    {
+        throw n;
+    }
+}
+
 int main()
 {
     int n;
@@ -8,23 +20,16 @@ int main()
     cin >> n;
     try
     {
-        if (cin.fail() == 1)
-        {
-            throw 'A';
-        }
-        if (n < 1 || n > 9)
-        {
-            throw n;
-        }
-    }
-    catch(int non_positive)
-    {
-        cout << "Error!! " << non_positive << " isn't positive number under 10" << endl;
-        return 0;
+        checking(n);
     }
     catch(char ch)
     {
         cout << "Error!! This isn't a number" << endl;
+        return 0;
+    }
+    catch(int non_positive)
+    {
+        cout << "Error!! " << non_positive << " isn't positive number under 10" << endl;
         return 0;
     }
     cout << n << " is a positive number under 10" << endl;
