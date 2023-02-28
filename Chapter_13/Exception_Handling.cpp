72eae5fcd1b70b4609df84ffef3b7960c1077ace
Jeongmo <wjdah1402@gmail.com>
2023-02-28 14:52:41 +0900

    Use exception class in previous commit

--- Chapter_13/Exception_Handling.cpp
+++ Chapter_13/Exception_Handling.cpp
@@ -1,37 +1,57 @@
 #include <iostream>
 using namespace std;
 
-void checking(int n)
+class Number_under_10 // Declare class
 {
-    if (cin.fail() == 1)
+private:
+    int n;
+public:
+    Number_under_10()
     {
-        throw 'A';
+        n = 0;
     }
-    if (n < 1 || n > 9)
+    class nonnumberError{}; //exception class
+    class nonrangeError{};
+    void Input() // Input function.
     {
-        throw n;
+        cout << "Enter a positive number under 10 : ";
+        cin >> n;
     }
-}
+    void checking() // Checking inputed number.
+    {
+        if (cin.fail() == 1)
+        {
+            throw nonnumberError();
+        }
+        if (n < 1 || n > 9)
+        {
+            throw nonrangeError();
+        }
+    }
+    int PrintN() // In order to print
+    {
+        return n;
+    }
+};
 
 int main()
 {
-    int n;
-    cout << "Enter a positive number under 10 : ";
-    cin >> n;
+    Number_under_10 Under_10;
     try
     {
-        checking(n);
+        Under_10.Input();
+        Under_10.checking();
     }
-    catch(char ch)
+    catch(Number_under_10::nonrangeError) //exception class's body
+    {
+        cout << "Error!! " << Under_10.PrintN() << " isn't positive number or under 10" << endl;
+        return 0;
+    }
+    catch(Number_under_10::nonnumberError)
     {
         cout << "Error!! This isn't a number" << endl;
         return 0;
     }
-    catch(int non_positive)
-    {
-        cout << "Error!! " << non_positive << " isn't positive number under 10" << endl;
-        return 0;
-    }
-    cout << n << " is a positive number under 10" << endl;
+    cout << Under_10.PrintN() << " is a positive number under 10" << endl;
     return 0;
 }
