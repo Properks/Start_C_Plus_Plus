3f0db71e02a830d128f7af0caabfa21c74751354
Jeongmo <wjdah1402@gmail.com>
2023-03-01 15:33:21 +0900

    Why and how use <vector> #27

--- Chapter_14/Standard_Templat_library.cpp
+++ Chapter_14/Standard_Templat_library.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector> // TODO: What is this
 using namespace std;
+
 int main()
 {
+    string ch;
+    string Str[] = {"Amy", "John", "Brian" ,"Jenny", "Tom"}; // 
+    vector<string> Newstr(&Str[0], &Str[5]); // TODO: Why did it use like this
+    vector<string>::iterator it; // TODO: What is this
+
+    cout << "Enter a string that you want to find : ";
+    cin >> ch;
+    it = find(Newstr.begin(), Newstr.end(), ch); // TODO: Why did it use like this
+    if(it == Newstr.end())
+    {
+        cout << ch << " isn't in this array..." << endl;
+    }
+    else
+    {
+        cout << ch << " is in this array!!" << endl;
+    }
     return 0;
 }
