2ef283eebbcc38cca0a130d35cdb96283317f47c
Jeongmo <wjdah1402@gmail.com>
2023-03-02 15:26:10 +0900

    Answer to question in previous commit

--- Chapter_14/Standard_Templat_library.cpp
+++ Chapter_14/Standard_Templat_library.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
-#include <vector> // TODO: What is this
+#include <vector> //It is container.
 using namespace std;
 
 int main()
 {
     string ch;
-    string Str[] = {"Amy", "John", "Brian" ,"Jenny", "Tom"}; // 
-    vector<string> Newstr(&Str[0], &Str[5]); // TODO: Why did it use like this
-    vector<string>::iterator it; // TODO: What is this
+    string Str[] = {"Amy", "John", "Brian" ,"Jenny", "Tom"};
+    vector<string> Newstr(&Str[0], &Str[5]); // Decalre variable with vector container.
+    vector<string>::iterator it; // Declare iterator. iterator is like pointer in vector container.
 
     cout << "Enter a string that you want to find : ";
     cin >> ch;
-    it = find(Newstr.begin(), Newstr.end(), ch); // TODO: Why did it use like this
+    it = find(Newstr.begin(), Newstr.end(), ch); // Use iterator(.begin(), .end()) in order to find char.
     if(it == Newstr.end())
     {
         cout << ch << " isn't in this array..." << endl;
