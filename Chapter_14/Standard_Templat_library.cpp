9f404b8d388f9edbc80d3f01083fb9ca5c4a3721
Jeongmo <wjdah1402@gmail.com>
2023-03-02 15:53:39 +0900

    Count how many char in a sentence

--- Chapter_14/Standard_Templat_library.cpp
+++ Chapter_14/Standard_Templat_library.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 #include <string>
-#include <vector> //It is container.
 using namespace std;
 
 int main()
 {
-    string ch;
-    string Str[] = {"Amy", "John", "Brian" ,"Jenny", "Tom"};
-    vector<string> Newstr(&Str[0], &Str[5]); // Decalre variable with vector container.
-    vector<string>::iterator it; // Declare iterator. iterator is like pointer in vector container.
+    char *Input, ch;
+    int num;
+    Input = new char[100];
+    cout << "Enter a sentence : ";
+    cin.getline(Input, 100, '\n');
 
-    cout << "Enter a string that you want to find : ";
+    cout << "Enter a spell that you want to find : ";
     cin >> ch;
-    it = find(Newstr.begin(), Newstr.end(), ch); // Use iterator(.begin(), .end()) in order to find char.
-    if(it == Newstr.end())
-    {
-        cout << ch << " isn't in this array..." << endl;
-    }
-    else
-    {
-        cout << ch << " is in this array!!" << endl;
-    }
+
+    num = count(&Input[0], &Input[strlen(Input) + 1], ch); // Count how many is 'char' in array.
+    cout << num << " numbers of " << ch << " is in this array!!" << endl;
+    delete [] Input;
     return 0;
 }
