5b4383914e2d2938be4cc8924915b088e979a98e
Jeongmo <wjdah1402@gmail.com>
2023-03-07 11:37:03 +0900

    Use queue in previous commit

--- Chapter_14/Standard_Templat_library.cpp
+++ Chapter_14/Standard_Templat_library.cpp
@@ -1,27 +1,11 @@
 #include <iostream>
-#include <stack> // stack (pop and push)
+#include <queue> // stack (pop and push)
 #include <string>
 using namespace std;
 
-void showstack(stack <char> stk)
-{
-    stack <char> Reverse;
-    while(!stk.empty())
-    {
-        Reverse.push(stk.top());
-        stk.pop();
-    }
-    while(!Reverse.empty()) // print all
-    {
-        cout << Reverse.top();
-        Reverse.pop();
-    }
-    cout << endl << endl;
-}
-
 int main()
 {
-    stack <char> palindrome;
+    queue <char> palindrome;
     char ch;
 
     cout << "Enter word or phrase (end : .): "; // Input
@@ -36,11 +20,10 @@ int main()
             palindrome.push(ch);
         }
     }
-    showstack(palindrome);
-    cout << "Reverse : ";
+    
     while(!palindrome.empty()) // print all
     {
-        ch = palindrome.top(); 
+        ch = palindrome.front(); 
         cout << ch;
         palindrome.pop();
     }
